2d556b838e62771995544f3e0b9c9aed277143a6
Tiara Brown <tiaradominique@gmail.com>
2021-11-20 17:45:04 -0500

    Added the containers for the recipes and reset the form/recipe each time the form was filled out. Close to MVP.

--- recipe.js
+++ recipe.js
@@ -3,9 +3,9 @@
 let recipeForm = document.getElementById("recipeFormRG");
 let recipeContainerRG = document.getElementById("recipeBoxRG");
 let recipeContainerTP = document.getElementById("storedRecipesTP");
-let entreRecipes = [];
-let drinkRecipes = [];
-let dessertRecipes = [];
+let entreRecipesArr = [];
+let drinkRecipesArr = [];
+let dessertRecipesArr = [];
 let currentMeal = [];
 
 function init(){
@@ -18,20 +18,23 @@ function handleSubmit(event){
     event.preventDefault();
     let drink = event.target.drinkOption.value;
     let dessert = event.target.dessertOption.value;
-    
-    if(drink == true){
+    // console.log(typeof(drink));
+    if(drink === 'true'){
         currentMeal[0] = generateRandomDrink();
+
     }else{
         currentMeal[0] = "You did not choose a drink.";
     }
 
-    if(dessert == true){
+    if(dessert === 'true'){
         currentMeal[2] = generateRandomDessert();
     }else{
         currentMeal[2] = "You did not choose a dessert.";
     }
 
     currentMeal[1] = generateRandomEntre();
+
+    console.log(currentMeal);
 
     renderMeal(currentMeal, recipeContainerRG);
 
@@ -46,46 +49,76 @@ function Recipe(recipeName, ingredientsList){
 }
 
 function drinkRecipe(recipeName, ingredientsList){
-    drinkRecipes.push(new Recipe(recipeName, ingredientsList));
+    drinkRecipesArr.push(new Recipe(recipeName, ingredientsList));
 }
 
 function entreRecipe(recipeName, ingredientsList){
-    entreRecipes.push(new Recipe(recipeName, ingredientsList));
+    entreRecipesArr.push(new Recipe(recipeName, ingredientsList));
 }
 
 function dessertRecipe(recipeName, ingredientsList){
-    dessertRecipes.push(new Recipe(recipeName, ingredientsList));
+    dessertRecipesArr.push(new Recipe(recipeName, ingredientsList));
 }
 
 function initializeRecipeObjects(){
     entreRecipe("PB&J", [["bread", "slices", 2], ["peanutButter", "tablespoon", 1], ["jelly", "tablespoon", 1]]);
+    entreRecipe("Ramen Noodles", [["noodle","block", 1], ["water", "ounces", 16], ["beef seasoning", "packet", 1]]);
+    // If this doesn't work you will add more context to the drink recipe
+    drinkRecipe("Water" , [[]]);
+    drinkRecipe("Sweet tea", [[]]);
+    dessertRecipe("Chocolate cake", [["egg", "chicken", 4], ["flour", "cups", 2], ["coacoa powder", "cups", 15]]);
+    dessertRecipe("Brownies",[["egg", "chicken", 1], ["flour", "cups", 5], ["coacoa powder", "cups", 14]] );
+    // console.log(drinkRecipesArr);
 }
 
 function generateRandomEntre(){
     //TODO: generate random 
+    let entreRecipeRandom = Math.round(Math.random() * (entreRecipesArr.length - 1));
+    return entreRecipesArr[entreRecipeRandom];
+
 }
 
 function generateRandomDrink(){
-    //TODO: generate random 
+    //TODO: generate random        
+       
+       let drinkRecipeRandom = Math.round(Math.random() * (drinkRecipesArr.length - 1));
+       return drinkRecipesArr[drinkRecipeRandom];
+       
 }
 
 function generateRandomDessert(){
     //TODO: generate random 
+    let dessertRecipeRandom = Math.round(Math.random() * (dessertRecipesArr.length - 1));
+    return dessertRecipesArr[dessertRecipeRandom];
 }
 
 
 function renderMeal(meal, parentContainer){
     //For each step of the array:
         // make a container for it 
+
+    parentContainer.innerHTML = ""
+    for(let i = 0; i < 3; i++){
+        let mealContainer = document.createElement('div');
+        mealContainer.id = "mealContainer";
+        let mealTitle = document.createElement('h2');
+        mealContainer.appendChild(mealTitle);
+        parentContainer.appendChild(mealContainer);
         //* check whether it's an object or a string; 
             //string, just set the content of the container to the string
             //object, run a special method to parse the object into something readable
-        // append the new object to parentContainer
+        let mealText = document.createElement('p');
+          // append the new object to parentContainer
+        mealContainer.appendChild(mealText);
+        if (typeof(meal[i]) === "string") {
+            mealText.textContent = meal[i];
+        } else {
+            let currentRecipe = meal[i];
+            mealTitle.textContent = currentRecipe.recipeName
+        }
+    }
         
 }
-// renderRecipe(some container, recipe object){
-//     take the recipe object, create a new div element top put it in, create text content of the new div with parsed recipe, append the readable recipe to "some container"
-// }
 
 
 
